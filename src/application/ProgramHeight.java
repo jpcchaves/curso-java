b472ec96589a9906970e1a506421e135d767f0f6
Joao Paulo <joaopaulodecastrochaves@hotmail.com>
2022-12-17 00:24:21 -0300

    challenge solutions

---
+++ src/application/ProgramHeight.java
@@ -0,0 +1,72 @@
+package application;
+
+import entities.Person;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ProgramHeight {
+  public static void main(String[] args) {
+    Locale.setDefault(Locale.US);
+    Scanner sc = new Scanner(System.in);
+
+    System.out.println("Quantas pessoas serão digitadas? ");
+    int N = sc.nextInt();
+
+    Person[] person = new Person[N];
+
+    for (int i = 0; i < N; i++) {
+      System.out.println("Nome: ");
+      sc.nextLine();
+      String name = sc.nextLine();
+      System.out.println("Idade: ");
+      int age = sc.nextInt();
+      System.out.println("Altura: ");
+      double height = sc.nextDouble();
+
+      person[i] = new Person(name, age, height);
+
+    }
+
+    for (int i = 0; i < N; i++) {
+      System.out.printf("Dados da %dª pessoa %n", i + 1);
+
+      System.out.println("Nome: " + person[i].getName());
+      System.out.println("Idade: " + person[i].getAge());
+      System.out.println("Altura: " + person[i].getHeight());
+
+
+    }
+    double allHeights = 0.0;
+    double avgHeight = 0.0;
+    int lessThanSixty = 0;
+    for (int i = 0; i < N; i++) {
+      if (person[i].getAge() < 16) {
+        lessThanSixty++;
+      }
+
+      allHeights += person[i].getHeight();
+    }
+
+    avgHeight = allHeights / N;
+
+    double lessThanSixtyPercentage = lessThanSixty / N;
+
+    System.out.printf("Média das idades: %.3f", avgHeight);
+    System.out.println();
+
+    System.out.print("Pessoas com menos de 16 anos: " +
+            String.format("%.1f", lessThanSixtyPercentage * 100)
+            + "%");
+    System.out.println();
+    for (int i = 0; i < N; i++) {
+      if (person[i].getAge() < 16) {
+        System.out.println(person[i].getName());
+      }
+    }
+
+    sc.close();
+  }
+}
+
+
---
+++ src/application/ProgramHotel.java
@@ -0,0 +1,40 @@
+package application;
+
+import entities.Rent;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ProgramHotel {
+  public static void main(String[] args) {
+    Locale.setDefault(Locale.US);
+    Scanner sc = new Scanner(System.in);
+
+    Rent[] vect = new Rent[10];
+
+    System.out.println("how many rooms will be rentend? ");
+    int n = sc.nextInt();
+
+    for (int i = 1; i <= n; i++) {
+      System.out.println("Rent #:" + i + ":");
+      System.out.print("Name: ");
+      sc.nextLine();
+      String name = sc.nextLine();
+      System.out.print("Email: ");
+      String email = sc.next();
+
+      System.out.print("Room: ");
+      int roomNum = sc.nextInt();
+
+      vect[roomNum] = new Rent(name, email);
+    }
+
+    System.out.println();
+    System.out.println("Busy rooms: ");
+    for (int i = 0; i < 10; i++) {
+      if (vect[i] != null) {
+        System.out.println(i + ": " + vect[i]);
+      }
+    }
+  }
+}
