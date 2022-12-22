a1105dec23058bc1f1e5f38247c2a1ce09ecd0e4
Joao Paulo <joaopaulodecastrochaves@hotmail.com>
2022-12-22 13:10:12 -0300

    lesson about inheritance and polymorphism

---
+++ src/application/Program1Bilion.java
@@ -0,0 +1,11 @@
+package application;
+
+public class Program1Bilion {
+  public static void main(String[] args) {
+    int i = 0;
+    while (i < 1000000000) {
+      i++;
+    }
+    System.out.println(i);
+  }
+}
