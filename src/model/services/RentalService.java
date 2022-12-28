fb5a456dc502b630e3f4907815289c79a259188e
Joao Paulo <joaopaulodecastrochaves@hotmail.com>
2022-12-28 08:45:02 -0300

    lesson about interfaces

--- src/model/services/RentalService.java
+++ src/model/services/RentalService.java
@@ -10,9 +10,9 @@ public class RentalService {
   private final Double pricePerHour;
   private final Double pricePerDay;
 
-  private final BrazilTaxService taxService;
+  private final TaxService taxService;
 
-  public RentalService(Double pricePerHour, Double pricePerDay, BrazilTaxService taxService) {
+  public RentalService(Double pricePerHour, Double pricePerDay, TaxService taxService) {
     this.pricePerHour = pricePerHour;
     this.pricePerDay = pricePerDay;
     this.taxService = taxService;
